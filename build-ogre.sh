a0160694a1676060c588a6d5b3407dcd4a78c496
Alexandru Artimon <aalecs@gmail.com>
2015-03-07 15:32:12 +0200

    OCHA-DAP/hdx-ckan#2383 - Integrate geodata preview with ogr2ogr on server   - update deployment scripts

--- build-ogre.sh
+++ build-ogre.sh
@@ -4,3 +4,5 @@ docker build -t teodorescuserban/hdx-ogre .
 
 docker run -P -p 5002:3000 -v `pwd`:/src -w /src --name ogre teodorescuserban/hdx-ogre npm install
 docker rm ogre
+docker run -P -p 5002:3000 -v `pwd`:/src -w /src --name ogre teodorescuserban/hdx-ogre npm install -g nodemon
+docker rm ogre
--- install-ogre.sh
+++ install-ogre.sh
@@ -1,1 +1,2 @@
+docker rm ogre
 docker run -d -P -p 5002:3000 -v `pwd`:/src -w /src --name ogre teodorescuserban/hdx-ogre nodejs run.js
